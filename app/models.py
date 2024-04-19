b1687bd0fd7e5a3daed1d20fa4290a61f6c382e6
Your Name <arundpi6gmail.com>
2024-04-19 16:35:23 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -5,14 +5,24 @@ from django.db import models
 class Topic(models.Model):
     Topic_Name=models.CharField(max_length=100,primary_key=True)
 
+    def __str__(self):
+        return self.Topic_Name
+
+
 
 class Webpage(models.Model):
     Topic_Name=models.ForeignKey(Topic,on_delete=models.CASCADE)
     Name=models.CharField(max_length=100)
     URL=models.URLField()
 
+    def __str__(self):
+        return self.Name
+
 
 class AccessRecord(models.Model):
     Name=models.ForeignKey(Webpage,on_delete=models.CASCADE)
     Date=models.DateField()
     Author=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Author
